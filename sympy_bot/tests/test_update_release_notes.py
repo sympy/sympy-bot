520713a4c1d6de610fc0dad9cc7b541266cea935
Aaron Meurer <asmeurer@gmail.com>
2018-07-15 23:37:04 -0600

    Add more release notes tests

--- sympy_bot/tests/test_update_release_notes.py
+++ sympy_bot/tests/test_update_release_notes.py
@@ -17,3 +17,43 @@ def test_existing_header():
     assert new_notes12.splitlines()[114:119] == ['', '* core', '  - core change 1 ([#123](../pull/123) by [@asmeurer](https://github.com/asmeurer) and [@certik](https://github.com/certik))', '', '  * Derivatives by a variable a symbolic number of times, like `diff(f(x), (x,']
 
     assert new_notes12.splitlines()[614:620] == ['* solvers', '  * solvers change 1 ([#123](../pull/123) by [@asmeurer](https://github.com/asmeurer) and [@certik](https://github.com/certik))', '', '  * solvers change 2 ([#123](../pull/123) by [@asmeurer](https://github.com/asmeurer) and [@certik](https://github.com/certik))', '', '  * Enable initial condition solving in `dsolve`']
+
+
+def test_new_header():
+    notes = """\
+
+## Authors
+"""
+
+    # Also makes sure they are inserted in the right order. 'other' should
+    # stay last in submodules.txt
+    changelogs = {'solvers': ['- solvers change'], 'other': ['- other changes']}
+    authors = ['asmeurer']
+    pr_number = '123'
+
+    new_notes = update_release_notes(notes, changelogs, pr_number, authors)
+
+    assert new_notes == """\
+
+* solvers
+  - solvers change ([#123](../pull/123) by [@asmeurer](https://github.com/asmeurer))
+
+* other
+  - other changes ([#123](../pull/123) by [@asmeurer](https://github.com/asmeurer))
+
+## Authors
+"""
+
+def test_error():
+    notes = ""
+
+    changelogs = {'solvers': ['* solvers change 1', '* solvers change 2'], 'core': ['- core change 1']}
+    authors = ['asmeurer', 'certik']
+    pr_number = '123'
+
+    try:
+        new_notes = update_release_notes(notes, changelogs, pr_number, authors)
+    except RuntimeError as e:
+        assert "## Authors" in e.args[0]
+    else:
+        raise AssertionError("Did not raise")
