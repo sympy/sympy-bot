1279e11c629a18233ff15e9bef9aa6f3e6a24162
Aaron Meurer <asmeurer@gmail.com>
2018-08-23 14:46:25 -0600

    Add format_change to __init__.py

--- sympy_bot/__init__.py
+++ sympy_bot/__init__.py
@@ -1,5 +1,5 @@
 from .changelog import (get_valid_headers, get_changelog,
-    get_release_notes_filename, update_release_notes)
+    get_release_notes_filename, update_release_notes, format_change)
 
 __all__ = ['get_valid_headers', 'get_changelog', 'get_release_notes_filename',
-    'update_release_notes']
+    'update_release_notes', 'format_change']
