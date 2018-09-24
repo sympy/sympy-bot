a5ee925ef4850fa49bc12f552313592ee107be55
Aaron Meurer <asmeurer@gmail.com>
2018-09-24 10:29:33 -0400

    Better wording in get message

--- sympy_bot/webapp.py
+++ sympy_bot/webapp.py
@@ -50,7 +50,7 @@ async def main_get(request):
         total = rate_limit.limit
         reset_datetime = rate_limit.reset_datetime
 
-    return web.Response(status=200, text=f"You have {remaining} of {total} GitHub API requests remaining. They will reset on {reset_datetime} (UTC), which is in {reset_datetime - datetime.datetime.now(datetime.timezone.utc)}.")
+    return web.Response(status=200, text=f"SymPy Bot has {remaining} of {total} GitHub API requests remaining. They will reset on {reset_datetime} (UTC), which is in {reset_datetime - datetime.datetime.now(datetime.timezone.utc)}.")
 
 @router.register("pull_request", action="opened")
 @router.register("pull_request", action="reopened")
