b2443dae85519af2b2577609407525f33a46daa1
Aaron Meurer <asmeurer@gmail.com>
2019-02-28 16:56:49 -0700

    Add a test for an error comment after merging with a failed status

--- sympy_bot/tests/test_webapp.py
+++ sympy_bot/tests/test_webapp.py
@@ -462,7 +462,7 @@ async def test_status_good_existing_comment(action):
 async def test_closed_with_merging(mocker, action):
     # Based on test_status_good_existing_comment
 
-    update_wiki_called_kwargs = ()
+    update_wiki_called_kwargs = {}
     def mocked_update_wiki(*args, **kwargs):
         nonlocal update_wiki_called_kwargs
         assert not args # All args are keyword-only
@@ -618,14 +618,13 @@ async def test_closed_with_merging(mocker, action):
     }
 
 
-
 @parametrize('action', ['closed'])
 @parametrize('exception', [RuntimeError('error message'),
                            CalledProcessError(1, 'cmd')])
-async def test_closed_with_merging_error(mocker, action, exception):
+async def test_closed_with_merging_update_wiki_error(mocker, action, exception):
     # Based on test_closed_with_merging
 
-    update_wiki_called_kwargs = ()
+    update_wiki_called_kwargs = {}
     def mocked_update_wiki(*args, **kwargs):
         nonlocal update_wiki_called_kwargs
         assert not args # All args are keyword-only
@@ -798,6 +797,171 @@ async def test_closed_with_merging_error(mocker, action, exception):
         'pr_number': pr_number,
         'authors': ['asmeurer', 'certik'],
     }
+
+
+
+@parametrize('action', ['closed'])
+async def test_closed_with_merging_bad_status_error(mocker, action):
+    # Based on test_closed_with_merging
+
+    update_wiki_called_kwargs = {}
+    def mocked_update_wiki(*args, **kwargs):
+        nonlocal update_wiki_called_kwargs
+        assert not args # All args are keyword-only
+        update_wiki_called_kwargs = kwargs
+
+    mocker.patch('sympy_bot.webapp.update_wiki', mocked_update_wiki)
+    mocker.patch.dict(os.environ, {"GH_AUTH": "TESTING TOKEN"})
+
+    event_data = {
+        'pull_request': {
+            'number': 1,
+            'state': 'open',
+            'merged': True,
+            'comments_url': comments_url,
+            'commits_url': commits_url,
+            'head': {
+                'user': {
+                    'login': 'asmeurer',
+                    },
+            },
+            'base': {
+                'repo': {
+                    'contents_url': contents_url,
+                    'html_url': html_url,
+                },
+            },
+            'body': invalid_PR_description,
+            'statuses_url': statuses_url,
+        },
+        'action': action,
+    }
+
+
+    commits = [
+        {
+            'author': {
+                'login': 'asmeurer',
+            },
+            'commit': {
+                'message': "A good commit",
+            },
+            'sha': 'a109f824f4cb2b1dd97cf832f329d59da00d609a',
+        },
+        {
+            'author': {
+                'login': 'certik',
+                },
+            'commit': {
+                'message': "A good commit",
+            },
+            'sha': 'a109f824f4cb2b1dd97cf832f329d59da00d609a',
+        },
+        # Test commits without a login
+        {
+            'author': {},
+            'commit': {
+                'message': "A good commit",
+            },
+            'sha': 'a109f824f4cb2b1dd97cf832f329d59da00d609a',
+        },
+    ]
+
+    # Has comment from sympy-bot
+    comments = [
+        {
+            'user': {
+                'login': 'sympy-bot',
+            },
+            'url': existing_comment_url,
+        },
+        {
+            'user': {
+                'login': 'asmeurer',
+            },
+        },
+        {
+            'user': {
+                'login': 'certik',
+            },
+        },
+    ]
+
+    getiter = {
+        commits_url: commits,
+        comments_url: comments,
+    }
+
+    getitem = {}
+    post = {
+        statuses_url: {},
+        comments_url: {
+            'html_url': comment_html_url,
+        },
+    }
+
+    patch = {
+        existing_comment_url: {
+            'html_url': comment_html_url,
+            'body': comment_body,
+            'url': existing_comment_url,
+        },
+    }
+
+    event = _event(event_data)
+
+    gh = FakeGH(getiter=getiter, getitem=getitem, post=post, patch=patch)
+
+    await router.dispatch(event, gh)
+
+    getitem_urls = gh.getitem_urls
+    getiter_urls = gh.getiter_urls
+    post_urls = gh.post_urls
+    post_data = gh.post_data
+    patch_urls = gh.patch_urls
+    patch_data = gh.patch_data
+
+    assert getiter_urls == list(getiter), getiter_urls
+    assert getitem_urls == list(getitem)
+    assert post_urls == [statuses_url, comments_url, statuses_url]
+    # Statuses data
+    assert len(post_data) == 3
+    assert post_data[0] == {
+        "state": "failure",
+        "target_url": comment_html_url,
+        "description": "The release notes check failed",
+        "context": "sympy-bot/release-notes",
+    }
+    assert post_data[1].keys() == {'body'}
+    error_message = post_data[1]['body']
+    assert ':rotating_light:' in error_message
+    assert 'ERROR' in error_message
+    assert 'https://github.com/sympy/sympy-bot/issues' in error_message
+    assert "The pull request was merged even though the release notes bot had a failing status." in error_message
+
+    assert post_data[2] == {
+        "state": "error",
+        "target_url": comment_html_url,
+        "description": "There was an error updating the release notes on the wiki.",
+        "context": "sympy-bot/release-notes",
+    }
+    # Comments data
+    assert patch_urls == [existing_comment_url]
+    assert len(patch_data) == 1
+    assert patch_data[0].keys() == {"body"}
+    comment = patch_data[0]["body"]
+    assert ":white_check_mark:" not in comment
+    assert ":x:" in comment
+    assert "new trig solvers" not in comment
+    assert "error" not in comment
+    assert "There was an issue" in comment
+    assert "https://github.com/sympy/sympy-bot" in comment
+    for line in invalid_PR_description:
+        assert line in comment
+    assert "good order" not in comment
+    assert "No release notes were found" in comment, comment
+
+    assert update_wiki_called_kwargs == {}
 
 
 @parametrize('action', ['opened', 'reopened', 'synchronize', 'edited'])
