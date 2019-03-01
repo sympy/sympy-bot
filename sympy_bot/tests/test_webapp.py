38217cba354e3e8a80c19c0a1208a8d0108119ce
Aaron Meurer <asmeurer@gmail.com>
2019-03-01 15:57:29 -0700

    Add a test for merging a PR with no changelog entry

--- sympy_bot/tests/test_webapp.py
+++ sympy_bot/tests/test_webapp.py
@@ -618,6 +618,155 @@ async def test_closed_with_merging(mocker, action):
     }
 
 
+
+@parametrize('action', ['closed'])
+async def test_closed_with_merging_no_entry(mocker, action):
+    # Based on test_status_good_existing_comment
+
+    update_wiki_called_kwargs = {}
+    def mocked_update_wiki(*args, **kwargs):
+        nonlocal update_wiki_called_kwargs
+        assert not args # All args are keyword-only
+        update_wiki_called_kwargs = kwargs
+
+    mocker.patch('sympy_bot.webapp.update_wiki', mocked_update_wiki)
+
+    event_data = {
+        'pull_request': {
+            'number': 1,
+            'state': 'open',
+            'merged': True,
+            'comments_url': comments_url,
+            'commits_url': commits_url,
+            'head': {
+                'user': {
+                    'login': 'asmeurer',
+                    },
+            },
+            'base': {
+                'repo': {
+                    'contents_url': contents_url,
+                    'html_url': html_url,
+                },
+            },
+            'body': valid_PR_description_no_entry,
+            'statuses_url': statuses_url,
+        },
+        'action': action,
+    }
+
+
+    commits = [
+        {
+            'author': {
+                'login': 'asmeurer',
+            },
+            'commit': {
+                'message': "A good commit",
+            },
+            'sha': 'a109f824f4cb2b1dd97cf832f329d59da00d609a',
+        },
+        {
+            'author': {
+                'login': 'certik',
+                },
+            'commit': {
+                'message': "A good commit",
+            },
+            'sha': 'a109f824f4cb2b1dd97cf832f329d59da00d609a',
+        },
+        # Test commits without a login
+        {
+            'author': {},
+            'commit': {
+                'message': "A good commit",
+            },
+            'sha': 'a109f824f4cb2b1dd97cf832f329d59da00d609a',
+        },
+    ]
+
+    # Has comment from sympy-bot
+    comments = [
+        {
+            'user': {
+                'login': 'sympy-bot',
+            },
+            'url': existing_comment_url,
+        },
+        {
+            'user': {
+                'login': 'asmeurer',
+            },
+        },
+        {
+            'user': {
+                'login': 'certik',
+            },
+        },
+    ]
+
+    version_file = {
+        'content': base64.b64encode(b'__version__ = "1.2.1.dev"\n'),
+        }
+
+    getiter = {
+        commits_url: commits,
+        comments_url: comments,
+    }
+
+    getitem = {
+        version_url: version_file,
+    }
+    post = {
+        statuses_url: {},
+    }
+
+    patch = {
+        existing_comment_url: {
+            'html_url': comment_html_url,
+            'body': comment_body,
+            'url': existing_comment_url,
+        },
+    }
+
+    event = _event(event_data)
+
+    gh = FakeGH(getiter=getiter, getitem=getitem, post=post, patch=patch)
+
+    await router.dispatch(event, gh)
+
+    getitem_urls = gh.getitem_urls
+    getiter_urls = gh.getiter_urls
+    post_urls = gh.post_urls
+    post_data = gh.post_data
+    patch_urls = gh.patch_urls
+    patch_data = gh.patch_data
+
+    assert getiter_urls == list(getiter), getiter_urls
+    assert getitem_urls == list(getitem)
+    assert post_urls == [statuses_url]
+    # Statuses data
+    assert post_data == [{
+        "state": "success",
+        "target_url": comment_html_url,
+        "description": "The release notes look OK",
+        "context": "sympy-bot/release-notes",
+    }]
+    # Comments data
+    assert patch_urls == [existing_comment_url]
+    assert len(patch_data) == 1
+    assert patch_data[0].keys() == {"body"}
+    comment = patch_data[0]["body"]
+    assert "No release notes entry will be added for this pull request." in comment
+    assert ":white_check_mark:" in comment
+    assert ":x:" not in comment
+    assert "error" not in comment
+    assert "https://github.com/sympy/sympy-bot" in comment
+    for line in valid_PR_description:
+        assert line in comment
+
+    assert update_wiki_called_kwargs == {}
+
 @parametrize('action', ['closed'])
 @parametrize('exception', [RuntimeError('error message'),
                            CalledProcessError(1, 'cmd')])
